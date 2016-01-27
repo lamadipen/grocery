6d59da25ad9fd8d5a64acb25cd68f742f6e96c04
Dipen Lama <lamadipen@yahoo.com>
2016-01-27 15:23:37 +0545

    Final version of code
    This is the final version of the code. (Program completed)

--- OnlineGrocery/src/PurchaseTest_bak_v2.java
+++ OnlineGrocery/src/PurchaseTest_bak_v2.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class PurchaseTest {
+public class PurchaseTest_bak_v2 {
 
 	// declare N as a constant.
 	// initialise N to a random number in the range 6 to 9.
@@ -21,9 +21,8 @@ public class PurchaseTest {
 	public static Purchase postcodeArray[]= new Purchase[N];
 	
 	//default constructor
-	public PurchaseTest() {		
+	public PurchaseTest_bak_v2() {		
 		//this is just for testing deleted after successfult testing all feature,
-		/**
 		postcodeArray[0] = new Purchase(4121); 
 		postcodeArray[1] = new Purchase(4122);
 		postcodeArray[2] = new Purchase(4120);
@@ -32,78 +31,64 @@ public class PurchaseTest {
 		purchaseAmountArray[0] = new Purchase(450.50);
 		purchaseAmountArray[1] = new Purchase(460.23);
 		purchaseAmountArray[2] = new Purchase(470.75);
-		purchaseAmountArray[3] = new Purchase(460.23);	
-		**/	
+		purchaseAmountArray[3] = new Purchase(460.23);		
 	}
-
+	
 	//Method to read, validate and store postcode and purchase amount for N customers
 	public  void addRecord(int customerCounter) throws IOException
 	{
 		int postcode;
 		double purchaseAmount;
-
-		int conform =0;
-	
-		do{
-			BufferedReader breader= new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Postcode: ");
-			postcode= Integer.parseInt(breader.readLine());
+		
+		BufferedReader breader= new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Postcode: ");
+		postcode= Integer.parseInt(breader.readLine());
+		
+		if(4121 >postcode)
+		{
+			System.out.print("Postcode must be greater than 4121");
+			System.out.print("Try again: ");			
+			addRecord(customerCounter);
+		}
+		else if(postcode > 4123)
+		{
+			System.out.print("Postcode must be less than 4123");
+			System.out.print("Try again: ");			
+			addRecord(customerCounter);
+		}
+		else
+		{
+			breader= new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Purchase Amount: ");
+			purchaseAmount= Double.parseDouble(breader.readLine());
 			
-			if(4121 >postcode)
+			if(purchaseAmount < 60)
 			{
-				System.out.print("Postcode must be greater than 4121");
-				System.out.print("Try again: ");							
+				System.out.print("Purchase Amount must be greater than $60");
+				System.out.print("Try again: ");					
+				addRecord(customerCounter);
 			}
-			else if(postcode > 4123)
+			else if(purchaseAmount > 500)
 			{
-				System.out.print("Postcode must be less than 4123");
-				System.out.print("Try again: ");							
+				System.out.print("Purchase Amount must be greater than $500");
+				System.out.print("Try again: ");						
+				addRecord(customerCounter);
 			}
 			else
 			{
-				conform = 1;				
-			}
-		}
-		while(4121 >postcode || postcode > 4123);
-
-	    if(conform == 1)
-	    {
-	    	System.out.println(conform);
-	    	do
-			{
-				BufferedReader breader2= new BufferedReader(new InputStreamReader(System.in));
-				breader2= new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("Purchase Amount: ");
-				purchaseAmount= Double.parseDouble(breader2.readLine());
+				System.out.print("Done: ");
+				System.out.print("purchaseAmount: "+purchaseAmount);
+				System.out.print("postcode: "+postcode);
 				
-				if(purchaseAmount < 60)
-				{
-					System.out.print("Purchase Amount must be greater than $60");
-					System.out.print("Try again: ");
-					continue;
-				}
-				else if(purchaseAmount > 500)
-				{
-					System.out.print("Purchase Amount must be greater than $500");
-					System.out.print("Try again: ");												
-				}	
-				else
-				{
-					conform = 2;
-				}
-			}
-			while(purchaseAmount < 60 || purchaseAmount > 500);			
-
-		    if(conform == 2)
-		    {
-		    	System.out.print("Done: ");
-		    	
 				purchaseAmountArray[customerCounter] = new Purchase(purchaseAmount);
-				postcodeArray[customerCounter] = new Purchase(postcode);												
-		    }
-	    }	
+				postcodeArray[customerCounter] = new Purchase(postcode);	
+				
+				System.out.println(purchaseAmountArray[1].getPurchaseAmount());
+			}			
+		}
+		
+		
 	}
-	
 	
 	//Method to display the total payable for all customers
 	public void displayTotalPayable()
@@ -157,17 +142,17 @@ public class PurchaseTest {
 		// object of type Purchase
 		Purchase purchaseObj = new Purchase();
 		
-		// object of type PurchaseTest
-		PurchaseTest purchaseTestObj = new PurchaseTest();
-		
-		int customerCount = 0;
+		int customerCount = 1;
 		int choice = 0;
 		
+		BufferedReader breader= new BufferedReader(new InputStreamReader(System.in));
+		
 		// While loop to continually display menu and read option
-		do
-		{
-			BufferedReader breader= new BufferedReader(new InputStreamReader(System.in));
-
+		while (customerCount <= N) {
+			//delete after testing
+			System.out.println("This customer no" + customerCount);
+			
+			
 			System.out.println("\n");
 
 			System.out.println("\n\t\t\t******Enter your choice******");
@@ -189,40 +174,36 @@ public class PurchaseTest {
 			switch(choice)
 			{
 				case 1: 
-					//while loop to add n number of customers into array					
-					customerCount =0;
-					while (customerCount < N) {					
-						purchaseTestObj.addRecord(customerCount);
-						customerCount++;
-					}
+					PurchaseTest_bak_v2 purchaseTestObj = new PurchaseTest_bak_v2();
+					purchaseTestObj.addRecord(customerCount);
 				break;
 
 				case 2: 
-					purchaseTestObj = new PurchaseTest();
+					purchaseTestObj = new PurchaseTest_bak_v2();
 					purchaseTestObj.displayTotalPayable();
 				break;
 
 				case 3: 
-					purchaseTestObj = new PurchaseTest();
+					purchaseTestObj = new PurchaseTest_bak_v2();
 					purchaseObj.displayAbove(purchaseTestObj.purchaseAmountArray,purchaseTestObj.postcodeArray);
 				break;
 
 				case 4:
-					purchaseTestObj = new PurchaseTest();
+					purchaseTestObj = new PurchaseTest_bak_v2();
 					purchaseObj.displayHighestPurchase(purchaseTestObj.purchaseAmountArray,purchaseTestObj.postcodeArray);
 				break;
 
 				case 5:					
-					purchaseTestObj = new PurchaseTest();					
+					purchaseTestObj = new PurchaseTest_bak_v2();					
 					purchaseObj.displayLowestPurchase(purchaseTestObj.purchaseAmountArray,purchaseTestObj.postcodeArray);				
 				break;
 
 				case 6:
-					purchaseTestObj = new PurchaseTest();					
+					purchaseTestObj = new PurchaseTest_bak_v2();					
 					purchaseObj.displayTotalPayableDescending(purchaseTestObj.purchaseAmountArray,purchaseTestObj.postcodeArray);
 				break;
 				case 7: 
-					purchaseTestObj = new PurchaseTest();
+					purchaseTestObj = new PurchaseTest_bak_v2();
 					purchaseObj.searchByPostcode(purchaseTestObj.purchaseAmountArray,purchaseTestObj.postcodeArray);
 					break;
 				case 8: 
@@ -234,8 +215,9 @@ public class PurchaseTest {
 					System.out.println("Invalid Input!");
 				}
 			}
-		}
-		while(choice!=8);
+
+			customerCount++;
+		}		
 	}
 	
 	
